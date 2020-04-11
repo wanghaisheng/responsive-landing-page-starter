5f0e2c24f1f13dd8b0d9ecf92192385feab67b24
Luke Oliff <luke@lukeoliff.com>
2020-04-11 14:17:00 +0100

    add community subscription form using netlify forms

--- components/CommunityUpdatesCard.vue
+++ components/CommunityUpdatesCard.vue
@@ -7,19 +7,21 @@
         </h3>
       </div>
       <div class="Vlt-card__content">
-        <p class="p-large">Subscribe to receive updates, tutorials, and news on Vonage communications APIs, libraries, SDKs, and developer tools.</p>
-        <div class="Vlt-form__element Vlt-form__element--big">
-          <div class="Vlt-composite">
-            <div class="Vlt-composite__wrapper Vlt-input">
-              <input type="text" placeholder="email@example.com" id="ex-input" value=""/>
-              <label for="ex-input">Email</label>
+        <form name="contact" netlify>
+          <p class="p-large">Subscribe to receive updates, tutorials, and news on Vonage communications APIs, libraries, SDKs, and developer tools.</p>
+          <div class="Vlt-form__element Vlt-form__element--big">
+            <div class="Vlt-composite">
+              <div class="Vlt-composite__wrapper Vlt-input">
+                <input type="text" placeholder="email@example.com" id="ex-input" value=""/>
+                <label for="ex-input">Email</label>
+              </div>
+              <div class="Vlt-composite__append">
+                <button class="Vlt-btn">Subscribe</button>
+              </div>
             </div>
-            <div class="Vlt-composite__append">
-              <button class="Vlt-btn">Subscribe</button>
-            </div>
+            <small class="Vlt-form__element__hint">By signing up to our communications blog, you accept our <a href="https://www.nexmo.com/privacy-policy" target="_blank">privacy policy</a>, which sets out how we use your data and the rights you have in respect of your data. You can opt out of receiving our updates by clicking the unsubscribe link in the email or by emailing us at <a href="mailto:privacy@nexmo.com" target="_blank">privacy@nexmo.com</a>.</small>
           </div>
-          <small class="Vlt-form__element__hint">By signing up to our communications blog, you accept our <a href="https://www.nexmo.com/privacy-policy" target="_blank">privacy policy</a>, which sets out how we use your data and the rights you have in respect of your data. You can opt out of receiving our updates by clicking the unsubscribe link in the email or by emailing us at <a href="mailto:privacy@nexmo.com" target="_blank">privacy@nexmo.com</a>.</small>
-        </div>
+        </form>
       </div>
     </div>
   </div>
