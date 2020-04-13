5016b1de0c1894b40f185912253c6b8e82146fa3
Luke Oliff <luke@lukeoliff.com>
2020-04-13 11:03:58 +0100

    add algolia export to function

--- scripts/algolia-export/algolia-export.js
+++ scripts/algolia-export/algolia-export.js
@@ -1,15 +1,43 @@
-// Docs on event and context https://www.netlify.com/docs/functions/#the-handler-method
+const algoliasearch = require('algoliasearch')
+const request = require('request')
+const StreamArray = require('stream-json/streamers/StreamArray')
+
+const errorHandler = (err) => {
+  context.fail(err)
+}
+
 exports.handler = async (event, context) => {
   try {
-    const subject = event.queryStringParameters.name || 'World'
-    return {
-      statusCode: 200,
-      body: JSON.stringify({ message: `Hello ${subject}`, env: process.env })
-      // // more keys you can return:
-      // headers: { "headerName": "headerValue", ... },
-      // isBase64Encoded: true,
-    }
+    const client = algoliasearch(process.env.ALGOLIA_ID, process.env.ALGOLIA_SECRET)
+    const index = client.initIndex(process.env.ALGOLIA_INDEX)
+    
+    const stream = request(`${process.env.URL}/output.json`).pipe(StreamArray.withParser())
+    let chunks = []
+    
+    stream
+      .on('data', ({ value }) => {
+        chunks.push(value)
+        if (chunks.length === 10000) {
+          stream.pause()
+          index
+            .saveObjects(chunks, { autoGenerateObjectIDIfNotExist: true })
+            .then(() => {
+              chunks = []
+              stream.resume()
+            })
+            .catch(errorHandler)
+        }
+      })
+      .on('end', () => {
+        if (chunks.length) {
+          index.saveObjects(chunks, { 
+            autoGenerateObjectIDIfNotExist: true
+          }).catch(errorHandler)
+          context.succeed()
+        }
+      })
+      .on('error', err => errorHandler(err))
   } catch (err) {
-    return { statusCode: 500, body: err.toString() }
+    errorHandler(err)
   }
 }
