19e2264c4bf5e5757df59f9e0ae39e83fcedd06e
Luke Oliff <luke@lukeoliff.com>
2020-04-14 22:26:30 +0100

    fix ruby page

--- pages/ruby.vue
+++ pages/ruby.vue
@@ -60,7 +60,7 @@ export default {
     return instantsearch
       .findResultsState({
         // find out which parameters to use here using ais-state-results
-        query: 'ruby ruby-on-rails rails',
+        query: 'ruby',
         hitsPerPage: 6
       })
       .then(() => ({
