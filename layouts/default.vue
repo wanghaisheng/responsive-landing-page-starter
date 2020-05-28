2d358a53c1d07701a5d02d06628bb21f01d95520
Luke Oliff <luke@lukeoliff.com>
2020-05-28 07:27:08 +0100

    add netlify identity script to all relevent pages

--- layouts/default.vue
+++ layouts/default.vue
@@ -15,5 +15,13 @@ export default {
     Footer,
     Header,
   },
+  
+  head() {
+    return {
+      script: [
+        { src: "https://identity.netlify.com/v1/netlify-identity-widget.js", defer: true },
+      ],
+    }
+  },
 }
 </script>
