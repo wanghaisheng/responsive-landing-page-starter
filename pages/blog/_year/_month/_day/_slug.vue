063676b16f935383385f1573afd4359afd94d4fe
Luke Oliff <luke@lukeoliff.com>
2020-06-18 23:21:51 +0100

    restyle blog read pages based on previous changes

--- pages/blog/_year/_month/_day/_slug.vue
+++ pages/blog/_year/_month/_day/_slug.vue
@@ -175,29 +175,63 @@ export default {
   border-radius: 6px;
 }
 
-/* .Blog__post .frontmatter-markdown {
+.Blog__post .frontmatter-markdown {
   padding: auto 50px;
 }
 
-.Blog__post .frontmatter-markdown >>> li,
-.Blog__post .frontmatter-markdown >>> p {
-  font-size: 18px;
-  line-height: 1.6;
+.Blog__post .nuxt-content >>> a,
+.Blog__post .nuxt-content >>> li,
+.Blog__post .nuxt-content >>> p {
+  font-size: 16px;
+  line-height: 1.55em;
 }
 
-.Blog__post .frontmatter-markdown >>> .Vlt-list {
-  margin-left: 16px;
+.Blog__post .nuxt-content >>> ol,
+.Blog__post .nuxt-content >>> ul {
+  list-style: none;
+  margin-bottom: 16px;
+  padding-left: 16px;
 }
 
-.Blog__post .frontmatter-markdown >>> h2 {
+
+.Blog__post .nuxt-content >>> ol {
+  counter-reset: list;
+  padding-left: 20px;
+}
+
+.Blog__post .nuxt-content >>> li {
+  margin-bottom: 0.2em;
+  position: relative;
+  margin-left: 24px;
+}
+
+.Blog__post .nuxt-content >>> ul li:before {
+  color: #000;
+  content: '•';
+  left: -16px;
+  position: absolute;
+  top: 0em;
+}
+
+.Blog__post .nuxt-content >>> ol li:before {
+  color: #000;
+  content: counter(list) ".";
+  counter-increment: list;
+  font-weight: 600;
+  left: -20px;
+  position: absolute;
+  top: 0em;
+}
+
+.Blog__post .nuxt-content >>> h2 {
   margin-top: 45px;
 }
 
-.Blog__post .frontmatter-markdown >>> h3 {
+.Blog__post .nuxt-content >>> h3 {
   margin-top: 35px;
 }
 
-.Blog__post .frontmatter-markdown >>> pre {
+.Blog__post .nuxt-content >>> pre {
   border-radius: 8px;
   padding: 1em;
   background: #131415;
@@ -208,12 +242,12 @@ export default {
   padding-left: 27px;
 }
 
-.Blog__post .frontmatter-markdown >>> pre code {
+.Blog__post .nuxt-content >>> pre code {
   background: #131415;
   color: #c2c4cc;
 }
 
-.Blog__post .frontmatter-markdown >>> p {
+.Blog__post .nuxt-content >>> p {
   text-align: justify;
   -webkit-hyphens: auto;
   -ms-hyphens: auto;
@@ -222,42 +256,74 @@ export default {
   word-break: normal;
 }
 
-.Blog__post .frontmatter-markdown >>> blockquote.Vlt-callout.Vlt-callout--tip {
+.Blog__post .nuxt-content >>> blockquote {
   margin: 24px auto;
+  background-color: #ffffff;
+  border-radius: 6px;
+  box-shadow: inset 0 0 0 1px #9b9da3;
+  display: -ms-flexbox;
+  display: flex;
+  opacity: 1;
+  overflow: hidden;
+  padding: 20px;
+  padding-left: 21px;
+  position: relative;
+  text-align: left;
+  transition: all 0.3s ease-out;
 }
 
-.Blog__post .frontmatter-markdown >>> p code {
+.Blog__post .nuxt-content >>> blockquote:before {
+  bottom: 0;
+  content: '';
+  left: 0;
+  position: absolute;
+  top: 0;
+  width: 5px;
+  background-color: #871fff;
+}
+
+.Blog__post .nuxt-content >>> blockquote p {
+  -ms-flex-item-align: center;
+  -ms-grid-row-align: center;
+  align-self: center;
+  -ms-flex: 2;
+  flex: 2;
+  margin-left: 4px;
+  word-break: break-word;
+}
+
+.Blog__post .nuxt-content >>> p code {
   border: 1px solid silver;
   background: #f9f9fa;
 }
 
-.Blog__post .frontmatter-markdown >>> .language-diff .token {
+.Blog__post .nuxt-content >>> .language-diff .token {
   width: 100%;
   display: inherit;
   white-space: pre-wrap;
 }
 
-.Blog__post .frontmatter-markdown >>> .language-diff .token.inserted {
+.Blog__post .nuxt-content >>> .language-diff .token.inserted {
   color: #e84545;
   background: #270404;
 }
 
-.Blog__post .frontmatter-markdown >>> .language-diff .token.deleted {
+.Blog__post .nuxt-content >>> .language-diff .token.deleted {
   color: #86d8b9;
   background: #021a10;
 }
 
-.Blog__post .frontmatter-markdown >>> p img {
+.Blog__post .nuxt-content >>> p img {
   display: block;
-  margin: 0 auto;
+  margin: 24px auto;
 }
 
 @media only screen and (max-width: 767px) {
-  .Blog__post .frontmatter-markdown >>> pre[class*="language-"] {
+  .Blog__post .nuxt-content >>> pre[class*="language-"] {
     margin: 24px 10px;
     padding-left: 12px;
   }
-} */
+}
 
 .Vlt-grid >>> .Author-col {
   flex: 0 0 66.66%;
