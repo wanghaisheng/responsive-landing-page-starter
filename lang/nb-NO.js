17c6c4cee8ec036d7431d754542f8ae6d3d2983b
JoeStone13 <jostein_97@hotmail.com>
2020-10-18 13:57:05 +0200

    added norwegian translations (#161)

---
+++ lang/nb-NO.js
@@ -0,0 +1,69 @@
+export default {
+  /**
+   * Layout strings
+   */
+  layout_default_careers_title: "VÆR FORSIKTIG I DETTE OMRÅDET",
+  layout_default_careers_body: "Men, etter som du er her, vi ansetter folk som vet sin vei rundt konsollen.",
+  layout_default_careers_link: "Finn en karierre hos https://developer.nexmo.com/careers",
+
+  /**
+   * Page strings
+   */
+  page_index_view_all_posts: 'Vis alle blogg artikler',
+  page_index_latest_posts: 'Siste artikler',
+
+  page_search_title: 'Søkeresultater',
+  page_search_placeholder: 'e.g. Send SMS i Node.js',
+  page_search_label: 'Søk væres eksiterende innhold...',
+  page_search_stalled_hint: 'Søkingen tar lengre tid enn normalt...',
+  page_search_no_results_title: 'Ingen resultater funnet',
+  page_search_no_results_message: 'Prøv å forbedre søket ditt :)',
+
+  page_authors_title: 'Alle forfatterene våres',
+  page_authors_team: 'Vonage lag medlemmer',
+  page_authors_spotlight: 'Spotlight forfattere',
+  page_authors_alumni: 'Vonage Alumni',
+  page_authors_other: 'Andre forfattere',
+
+  page_blog_title: 'Utviklerinnhold fra arkivene',
+  page_blog_breadcrumb: 'Blogg',
+
+  page_blog_yearmonthday_title: 'Utviklerinnhold fra',
+
+  page_categorytag_title: 'Innlegg fra laget Vonage',
+
+  /**
+   * Component strings
+   */
+  component_search_hero_title: 'Utviklerinnhold fra teamet på',
+  component_search_hero_placeholder: 'e.g. Send SMS i Node.js',
+  component_search_hero_label: 'Søk i vårt eksisterende innhold...',
+  component_search_hero_stalled_hint: 'Søk tar lengre tid enn vanlig...',
+
+  component_header_strapline: 'Vonage :: Now we\'re talking',
+  component_header_badge: 'Lær',
+  component_header_slack_button: 'Bli med i Slack-en væres',
+  component_header_signup_button: 'Meld deg på',
+  component_header_follow_button: 'Følg oss',
+
+  component_footer_strapline: 'Vonage :: Now we\'re talking',
+  component_footer_legal: 'Lovlig',
+  component_footer_privacy: 'Personvern',
+  component_footer_copyright: 'Opphavsrett',
+  component_footer_patents: 'Patenter',
+  component_footer_service: 'SERVICE',
+  component_footer_partners: 'Partnere',
+  component_footer_systemstatus: 'System status',
+  component_footer_support: 'Brukerstøtte',
+  component_footer_referrals: 'Henvisninger',
+  component_footer_corporate: 'FIRMA',
+  component_footer_investors: 'Investorer',
+  component_footer_events: 'Arrangementer',
+  component_footer_careers: 'Karrierer',
+  component_footer_social: 'SOSIAL',
+  component_footer_facebook: 'Facebook',
+  component_footer_linkedin: 'LinkedIn',
+  component_footer_twitter: 'Twitter',
+  component_footer_youtube: 'YouTube',
+  component_footer_rssfeed: 'RSS Feed',
+}
