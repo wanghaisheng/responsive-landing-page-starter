8f48350148be593cc3338b67c20e72e2fcb77395
@lukeocodes <luke@lukeoliff.com>
2021-02-15 11:04:17 +0000

    feat(devtv): add icon and change selected colour

--- pages/devtv/shows.vue
+++ pages/devtv/shows.vue
@@ -8,9 +8,20 @@
         <div class="flex mt-3 md:mt-0 md:absolute md:top-3 md:right-0">
           <button
             type="button"
-            class="inline-flex items-center px-4 py-2 ml-3 text-sm font-medium text-white border border-transparent rounded-md shadow-sm bg-purple-dark hover:bg-purple-darker focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-purple"
+            class="inline-flex items-center px-4 py-2 ml-3 text-sm font-medium text-white border border-transparent rounded-md shadow-sm bg-brand-twitch"
           >
-            [icon] Watch on Twitch
+            <svg
+              role="img"
+              xmlns="http://www.w3.org/2000/svg"
+              viewBox="0 0 24 24"
+              class="w-4 h-4 mr-2 fill-current"
+            >
+              <title>Twitch icon</title>
+              <path
+                d="M11.571 4.714h1.715v5.143H11.57zm4.715 0H18v5.143h-1.714zM6 0L1.714 4.286v15.428h5.143V24l4.286-4.286h3.428L22.286 12V0zm14.571 11.143l-3.428 3.428h-3.429l-3 3v-3H6.857V1.714h13.714Z"
+              />
+            </svg>
+            Watch on Twitch
           </button>
         </div>
       </div>
@@ -38,7 +49,7 @@
             </nuxt-link>
             <nuxt-link
               to="/devtv/shows/"
-              class="px-1 pb-4 text-sm font-medium border-b-2 text-purple-dark border-purple-dark whitespace-nowrap"
+              class="px-1 pb-4 text-sm font-medium border-b-2 text-green-dark border-green-dark whitespace-nowrap"
               aria-current="page"
             >
               Shows
